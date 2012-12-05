4fd21d8f31d879918ee76a4b4c282624c0a36e53
Alexander Sapountzis <asap@briteideas.com>
2012-12-04 19:30:05 -0500

    Add route for adding new chirps

--- chirpy/views.py
+++ chirpy/views.py
@@ -7,7 +7,7 @@ views
 """
 
 from . import app
-from flask import render_template
+from flask import render_template, url_for
 
 chirps = [
     {'name': 'stark', 'chirp':'Winter is Coming'},
@@ -18,3 +18,8 @@ chirps = [
 @app.route('/')
 def index():
     return render_template('index.html', chirps=chirps)
+
+
+@app.route('/add/')
+def add():
+    return render_template('add.html', chirps=chirps)
