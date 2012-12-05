892e06a674d5b5bc4b211e4b0b5ef47777104602
Alexander Sapountzis <asap@briteideas.com>
2012-12-04 19:21:03 -0500

    We gots Chirps!

--- chirpy/views.py
+++ chirpy/views.py
@@ -9,7 +9,12 @@ views
 from . import app
 from flask import render_template
 
+chirps = [
+    # {'name': 'stark', 'chirp':'Winter is Coming'},
+    # {'name': 'fury', 'chirp':'...'},
+]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', chirps=chirps)
